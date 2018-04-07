2f5318e5ff24afae91b27b8636d1f2049d37b3f5
admin <admin@admins-macbook-pro.local>
2018-04-07 16:30:29 +0530

    Added someValue string constant which prints when the view load.

--- TestGitProject/ViewController.swift
+++ TestGitProject/ViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let someValue = "Hello Friends"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        print(someValue)
     }
 
     override func didReceiveMemoryWarning() {
